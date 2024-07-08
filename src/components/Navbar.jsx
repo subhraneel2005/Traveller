296894b53fcad9c1f883aec402a55248cf65a4b2
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-07-08 22:47:09 +0530

    implemented Clerk Authentication ALL BY MYSELF, NO HELP TAKEN

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
+"use client"
+
 import Link from 'next/link';
+import { UserButton, useUser } from '@clerk/nextjs';
+import { usePathname } from 'next/navigation';
 
 function Navbar() {
+
+  const path = usePathname();
+  const { user, isSignedIn } = useUser();
+
   return (
     <div className="flex justify-between py-6 px-10 bg-transparent w-full z-20 top-0">
       <h1 id="heroText" className="text-3xl font-extrabold text-sky-300 cursor-pointer">
@@ -12,6 +20,9 @@ function Navbar() {
         <Link href="/destinations" className="text-gray-100 text-underline font-bold text-[16px]">Destinations</Link>
         <Link href="/contacts" className="text-gray-100 text-underline font-bold text-[16px]">Contact</Link>
       </div>
+      {
+          isSignedIn ? <UserButton/> : 
+        <Link href={"/sign-in"} className='cursor-pointer text-sky-300 text-underline font-bold text-[16px]'><button>Login</button></Link>}
     </div>
   );
 }
